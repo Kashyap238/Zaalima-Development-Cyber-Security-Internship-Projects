b7daff54c781a3f350debec1648da6c2a29c02c2
Vineet375 <vineetkhatri97@gmail.com>
2026-06-03 16:02:49 +0530

    Added API authentication module

---
+++ Project 1 ( Enterprise LLM & GenAI Security Gateway  (AI Security) )/src/api.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from security_gateway import SecurityGateway
+from fastapi import FastAPI, Header, HTTPException
+
+app = FastAPI()
+API_KEY = "zaalimasecurity123"
+gateway = SecurityGateway()
+
+@app.get("/")
+def home():
+    return {"message": "Enterprise LLM Security Gateway Running"}
+
+@app.post("/analyze")
+def analyze(
+    prompt: str,
+    x_api_key: str = Header(None)
+):
+
+    if x_api_key != API_KEY:
+        raise HTTPException(
+            status_code=401,
+            detail="Unauthorized"
+        )
+
+    findings = gateway.detect_pii(prompt)
+
+    findings.extend(
+        gateway.detect_prompt_injection(prompt)
+    )
+
+    sanitized = gateway.sanitize_prompt(prompt)
+
+    return {
+        "original": prompt,
+        "sanitized": sanitized,
+        "findings": findings
+    }
