013df8e243e3e97750227643846823f1aa866ce0
Vineet375 <175447242+Vineet375@users.noreply.github.com>
2026-06-17 19:28:28 +0530

    Implemented brute force attack detection

---
+++ Project 2 ( Identity Threat Detection and Response  (ITDR) Platform)/src/brute_forcs_detection.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+
+def detect_brute_force():
+
+    current_dir = os.path.dirname(__file__)
+
+    csv_path = os.path.join(
+        current_dir,
+        "..",
+        "data",
+        "identity_events.csv"
+    )
+
+    data = pd.read_csv(csv_path)
+
+    failed_logins = {}
+
+    for _, row in data.iterrows():
+
+        if row["event"] == "Failed Login":
+
+            user = row["user"]
+
+            failed_logins[user] = (
+                failed_logins.get(user, 0) + 1
+            )
+
+            if failed_logins[user] >= 5:
+
+                print(
+                    f"Brute Force Attack Detected: {user}"
+                )
+
+if __name__ == "__main__":
+    detect_brute_force()
