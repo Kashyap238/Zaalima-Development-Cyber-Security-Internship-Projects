3d6c7cdf96b4ee64f0c17d0bcae248830453e974
Vineet375 <175447242+Vineet375@users.noreply.github.com>
2026-06-19 20:17:21 +0530

    Implemented ML based anomaly detection

---
+++ Project 2 ( Identity Threat Detection and Response  (ITDR) Platform)/src/anamoly_detection.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import os
+
+from sklearn.ensemble import IsolationForest
+
+def detect_anomalies():
+
+    current_dir = os.path.dirname(__file__)
+
+    csv_path = os.path.join(
+        current_dir,
+        "..",
+        "data",
+        "identity_events.csv"
+    )
+
+    data = pd.read_csv(csv_path)
+
+    data["event_id"] = range(
+        len(data)
+    )
+
+    X = data[["event_id"]]
+
+    model = IsolationForest(
+        contamination=0.2,
+        random_state=42
+    )
+
+    predictions = model.fit_predict(X)
+
+    data["anomaly"] = predictions
+
+    anomalies = data[
+        data["anomaly"] == -1
+    ]
+
+    print(
+        "\n=== Anomalous Events ===\n"
+    )
+
+    print(
+        anomalies[
+            ["user", "event"]
+        ]
+    )
+
+if __name__ == "__main__":
+    detect_anomalies()
